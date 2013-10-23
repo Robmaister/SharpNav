37df8df3615f4b076c6449ab687d1ea607c22a13
Samuel Yuan <yuans@rpi.edu>
2013-10-22 21:30:12 -0400

    Added filter functions to make some regions unwalkable and others walkable.
    -Low lying regions, such as stairs and curbs, should be made walkable
    -Spans with low clearance above them should be unwalkable
    -Ledges should be unwalkable

--- SharpNavTests/HeightfieldTests.cs
+++ SharpNavTests/HeightfieldTests.cs
@@ -78,5 +78,60 @@ namespace SharpNavTests
             var hf = new Heightfield(Vector3.Zero, Vector3.One, 0.5f, 0.5f);
             Assert.Throws<IndexOutOfRangeException>(() => { var c = hf[5]; });
         }
+
+        [Test]
+        public void Filter_LowHangingWalkable_Success()
+        {
+            var hf = new Heightfield(Vector3.Zero, Vector3.One, 0.5f, 0.02f);
+            var span = new Heightfield.Span(10, 15);
+            var span2 = new Heightfield.Span(16, 20);
+            
+            span.Area = Heightfield.AREA_WALKABLE;
+            span2.Area = Heightfield.AREA_NULL;
+            hf[0].AddSpan(span);
+            hf[0].AddSpan(span2);
+
+            hf.filterLowHangingWalkableObstacles(20);
+
+            Assert.AreEqual(hf[0].Spans[0].Area, hf[0].Spans[1].Area);
+        }
+
+        [Test]
+        public void Filter_LowHangingWalkable_Fail()
+        {
+            var hf = new Heightfield(Vector3.Zero, Vector3.One, 0.5f, 0.02f);
+            var span = new Heightfield.Span(1, 2);
+            var span2 = new Heightfield.Span(10, 20);
+
+            span.Area = Heightfield.AREA_WALKABLE;
+            span2.Area = Heightfield.AREA_NULL;
+            hf[2].AddSpan(span);
+            hf[2].AddSpan(span2);
+
+            //walkable step cannot cover the gap (difference between span2 maximum and span 1 maximum) so fail
+            hf.filterLowHangingWalkableObstacles(10);
+            Assert.AreNotEqual(hf[0, 1].Spans[0].Area, hf[0, 1].Spans[1].Area);
+        }
+
+        [Test]
+        public void Filter_WalkableLowHeight_Success()
+        {
+            var hf = new Heightfield(Vector3.Zero, Vector3.One, 0.5f, 0.02f);
+            var span = new Heightfield.Span(10, 20);
+            var span2 = new Heightfield.Span(25, 30);
+
+            span.Area = Heightfield.AREA_WALKABLE;
+            span2.Area = Heightfield.AREA_WALKABLE;
+            hf[0].AddSpan(span);
+            hf[0].AddSpan(span2);
+
+            //too low to walk through. there is only a gap of 5 units to walk through,
+            //but at least 15 units is needed
+            hf.filterWalkableLowHeightSpans(15);
+
+            //so one span is unwalkable and the other is fine
+            Assert.AreEqual(hf[0].Spans[0].Area, Heightfield.AREA_NULL);
+            Assert.AreEqual(hf[0].Spans[1].Area, Heightfield.AREA_WALKABLE);
+        }
 	}
 }
