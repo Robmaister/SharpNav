b02658db7f885d15a74bfdc47af155cec72e74b7
Samuel Yuan <yuans@rpi.edu>
2014-01-18 15:53:19 -0500

    In NodePool, added Dictionary<int, Node> to keep track of node id. Used a list, instead of array, to keep track of the nodes.

--- SharpNav/NodePool.cs
+++ SharpNav/NodePool.cs
@@ -15,61 +15,32 @@ namespace SharpNav
 	/// </summary>
 	public class NodePool
 	{
-		private Node[] m_nodes;
-		private int[] m_first;
-		private int[] m_next;
+		private List<Node> m_nodes;
+		private Dictionary<int, Node> nodeDict;
 		private int m_maxNodes;
-		private int m_hashSize;
-		private int m_nodeCount;
-
-		public const int NULL_IDX = ~0;
+		private static int m_hashSize;
 
 		public NodePool(int maxNodes, int hashSize)
 		{
 			m_maxNodes = maxNodes;
 			m_hashSize = hashSize;
 
-			m_nodes = new Node[m_maxNodes];
-			m_next = new int[m_maxNodes];
-			m_first = new int[hashSize];
-
-			for (int i = 0; i < hashSize; i++)
-				m_first[i] = 0xff;
-			for (int i = 0; i < m_maxNodes; i++)
-				m_next[i] = 0xff;
-		}
-
-		public int HashRef(int a)
-		{
-			a += ~(a << 15);
-			a ^= a >> 10;
-			a += a << 3;
-			a ^= a >> 6;
-			a += ~(a << 11);
-			a ^= a >> 16;
-
-			return a;
+			m_nodes = new List<Node>(m_maxNodes);
+			nodeDict = new Dictionary<int, Node>(new IntNodeIdComparer());
 		}
 
 		public void Clear()
 		{
-			for (int i = 0; i < m_hashSize; i++)
-				m_first[i] = 0xff;
-
-			m_nodeCount = 0;
+			m_nodes.Clear();
+			nodeDict.Clear();
 		}
 
 		public Node FindNode(int id)
 		{
-			int bucket = HashRef(id) & (m_hashSize - 1);
-			int i = m_first[bucket];
-
-			while (i != NULL_IDX)
+			Node node;
+			if (nodeDict.TryGetValue(id, out node))
 			{
-				if (m_nodes[i].id == id)
-					return m_nodes[i];
-
-				i = m_next[i];
+				return node;
 			}
 
 			return null;
@@ -77,40 +48,26 @@ namespace SharpNav
 
 		public Node GetNode(int id)
 		{
-			int bucket = HashRef(id) & (m_hashSize - 1);
-			int i = m_first[bucket];
-
-			while (i != NULL_IDX)
+			Node node;
+			if (nodeDict.TryGetValue(id, out node))
 			{
-				if (i >= MaxNodes)
-					break;
-
-				if (m_nodes[i] == null)
-					break;
-
-				if (m_nodes[i].id == id)
-					return m_nodes[i];
-
-				i = m_next[i];
+				return node;
 			}
 
-			if (m_nodeCount >= m_maxNodes)
+			if (m_nodes.Count >= m_maxNodes)
 				return null;
 
-			i = m_nodeCount;
-			m_nodeCount++;
+			Node newNode = new Node();
+			newNode.pidx = 0;
+			newNode.cost = 0;
+			newNode.total = 0;
+			newNode.id = id;
+			newNode.flags = 0;
+			
+			m_nodes.Add(newNode);
+			nodeDict.Add(id, newNode);
 
-			m_nodes[i] = new Node();
-			m_nodes[i].pidx = 0;
-			m_nodes[i].cost = 0;
-			m_nodes[i].total = 0;
-			m_nodes[i].id = id;
-			m_nodes[i].flags = 0;
-
-			m_next[i] = m_first[bucket];
-			m_first[bucket] = i;
-
-			return m_nodes[i];
+			return newNode;
 		}
 
 		public int GetNodeIdx(Node node)
@@ -118,7 +75,7 @@ namespace SharpNav
 			if (node == null)
 				return 0;
 
-			for (int i = 0; i < m_nodes.Length; i++)
+			for (int i = 0; i < m_nodes.Count; i++)
 			{
 				if (m_nodes[i] == node)
 					return i + 1;
@@ -129,23 +86,30 @@ namespace SharpNav
 
 		public Node GetNodeAtIdx(int idx)
 		{
-			if (idx <= 0 || idx > m_maxNodes)
+			if (idx <= 0 || idx > m_nodes.Count)
 				return null;
 
-			return m_nodes[idx - 1];
+			return m_nodes[idx - 1]; 
 		}
 
-		public int MaxNodes { get { return m_maxNodes; } }
-		public int HashSize { get { return m_hashSize; } }
-
-		public int GetFirst(int bucket)
+		private class IntNodeIdComparer : IEqualityComparer<int>
 		{
-			return m_first[bucket];
-		}
+			public bool Equals(int left, int right)
+			{
+				return left == right;
+			}
 
-		public int GetNext(int i)
-		{
-			return m_next[i];
+			public int GetHashCode(int obj)
+			{
+				obj += ~(obj << 15);
+				obj ^= obj >> 10;
+				obj += obj << 3;
+				obj ^= obj >> 6;
+				obj += ~(obj << 11);
+				obj ^= obj >> 16;
+
+				return obj & (m_hashSize - 1);
+			}
 		}
 	}
 }
