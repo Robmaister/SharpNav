a446f77cf5f4d5b205c1b727f4794222b5eb6423
Samuel Yuan <yuans@rpi.edu>
2014-03-21 14:30:13 -0400

    Added tests for PointToSegment2DSquared in MathHelper

--- SharpNavTests/MathHelperTests.cs
+++ SharpNavTests/MathHelperTests.cs
@@ -153,5 +153,51 @@ namespace SharpNavTests
 			//This test expects -33, even though the result is undefined
 			Assert.AreEqual(num, float.NaN);
 		}
+
+		[Test]
+		public void PointToSegment2DSquared_NoVectors()
+		{
+			//point is (0, 0), segment is (0, 1) to (1,0)
+			float dist = MathHelper.Distance.PointToSegment2DSquared(0, 0, 0, 1, 1, 0);
+
+			//safe floating value comparison
+			Assert.AreEqual(dist + float.Epsilon, 0.5f);
+		}
+
+		[Test]
+		public void PointToSegment2DSquared_Vectors()
+		{
+			//the point
+			Vector3 pt = new Vector3(0, 0, 0);
+
+			//the segment
+			Vector3 p = new Vector3(0, 0, 1);
+			Vector3 q = new Vector3(1, 0, 0);
+
+			float dist = MathHelper.Distance.PointToSegment2DSquared(ref pt, ref p, ref q);
+
+			//safe floating value comparison
+			Assert.AreEqual(dist + float.Epsilon, 0.5f);
+		}
+
+		[Test]
+		public void PointToSegment2DSquared_VectorsWithParameters()
+		{
+			//the point
+			Vector3 pt = new Vector3(0, 0, 0);
+
+			//the segment
+			Vector3 p = new Vector3(0, 0, 1);
+			Vector3 q = new Vector3(1, 0, 0);
+
+			//the paramater t
+			float t;
+
+			float dist = MathHelper.Distance.PointToSegment2DSquared(ref pt, ref p, ref q, out t);
+
+			//safe floating value comparison
+			Assert.AreEqual(dist + float.Epsilon, 0.5f);
+			Assert.AreEqual(t + float.Epsilon, 0.5f);
+		}
 	}
 }
