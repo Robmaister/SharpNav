c76868b080963303fffed6ad76669a44f90bf39f
Samuel Yuan <yuans@rpi.edu>
2014-04-08 13:21:00 -0400

    Added comments for the PriorityQueue class

--- SharpNav/Collections/Generic/PriorityQueue.cs
+++ SharpNav/Collections/Generic/PriorityQueue.cs
@@ -23,6 +23,10 @@ namespace SharpNav.Collections.Generic
 		private int capacity;
 		private int size;
 
+		/// <summary>
+		/// Initialize a new priority queue with a given capacity of size n.
+		/// </summary>
+		/// <param name="n">Maximum number of nodes that can be stored</param>
 		public PriorityQueue(int n)
 		{
 			capacity = n;
@@ -46,16 +50,28 @@ namespace SharpNav.Collections.Generic
 			}
 		}
 
+		/// <summary>
+		/// Remove all the elements from the priorirty queue.
+		/// </summary>
 		public void Clear()
 		{
 			size = 0;
 		}
 
+		/// <summary>
+		/// Return the node at the top of the heap.
+		/// </summary>
+		/// <returns></returns>
 		public T Top()
 		{
 			return heap[0];
 		}
 
+		/// <summary>
+		/// Remove the node at the top of the heap. Then, move the bottommost node to the top and trickle down
+		/// until the nodes are in order.
+		/// </summary>
+		/// <returns></returns>
 		public T Pop()
 		{
 			T result = heap[0];
@@ -64,6 +80,10 @@ namespace SharpNav.Collections.Generic
 			return result;
 		}
 
+		/// <summary>
+		/// Add the node at the bottom of the heap and move it up until the nodes ae in order.
+		/// </summary>
+		/// <param name="node">The node to add</param>
 		public void Push(T node)
 		{
 			size++;
@@ -75,6 +95,10 @@ namespace SharpNav.Collections.Generic
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Change the value of the node, which may involve some swapping of elements to maintain heap order.
+		/// </summary>
+		/// <param name="node">The node to modify</param>
 		public void Modify(T node)
 		{
 			for (int i = 0; i < size; i++)
@@ -120,6 +144,11 @@ namespace SharpNav.Collections.Generic
 			return GetEnumerator();
 		}
 
+		/// <summary>
+		/// While going up a priority queue, keep swapping elements until the element reaches the top.
+		/// </summary>
+		/// <param name="i">Index of current node</param>
+		/// <param name="node">The node itself</param>
 		private void BubbleUp(int i, T node)
 		{
 			int parent = (i - 1) / 2;
@@ -134,12 +163,19 @@ namespace SharpNav.Collections.Generic
 			heap[i] = node;
 		}
 
+	
+		/// <summary>
+		/// While moving down the priority queue, keep swapping elements.
+		/// </summary>
+		/// <param name="i">Index of current node</param>
+		/// <param name="node">The node itself</param>
 		private void TrickleDown(int i, T node)
 		{
 			int child = (i * 2) + 1;
 
 			while (child < size)
 			{
+				//determine which child element has a smaller cost 
 				if (((child + 1) < size) && (heap[child].Cost > heap[child + 1].Cost))
 				{
 					child++;
