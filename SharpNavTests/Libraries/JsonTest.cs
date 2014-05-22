597899170c12a1596b966bc80bc2a59635442cc6
Ryan Lin <rlincc@gmail.com>
2014-05-21 20:27:52 -0400

    Add test case to ensure that JSON.Net is working properly

---
+++ SharpNavTests/Libraries/JsonTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+using SharpNav;
+using SharpNavTests;
+using SharpNav.Geometry;
+
+namespace SharpNavTests.Libraries
+{
+    [TestFixture]
+    class JsonTest
+    {
+        [Test]
+        public void JsonNet_Test()
+        {
+            // Simple test to make sure that the JSON.Net library is working
+            Triangle3 triangle = new Triangle3(new Vector3(3), new Vector3(4), new Vector3(5));
+            string output = JsonConvert.SerializeObject(triangle);
+
+        }
+    }
+}
