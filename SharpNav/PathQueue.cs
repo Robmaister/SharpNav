9798a632aa796ff949b9bed014a5033042089893
Samuel Yuan <yuans@rpi.edu>
2014-05-29 20:57:53 -0400

    Implemented all the methods in PathQueue
    The PathQueue class can keep track of multiple paths for the Crowd

--- SharpNav/PathQueue.cs
+++ SharpNav/PathQueue.cs
@@ -34,6 +34,13 @@ namespace SharpNav
 		private int queueHead;
 		private NavMeshQuery navquery;
 
+		public enum Status
+		{
+			FAILURE = 1,
+			SUCCESS = 2,
+			IN_PROGRESS = 3
+		}
+
 		public PathQueue(int maxPathSize, int maxSearchNodeCount, ref TiledNavMesh nav)
 		{
 			this.navquery = new NavMeshQuery(nav, maxSearchNodeCount);
@@ -42,10 +49,154 @@ namespace SharpNav
 			for (int i = 0; i < MAX_QUEUE; i++)
 			{
 				queue[i].Reference = PATHQ_INVALID;
-				queue[i].Path = new uint[maxPathSize];
+				queue[i].Path = new List<int>(maxPathSize);
 			}
 
 			this.queueHead = 0;
+		}
+
+		public void Update(int maxIters)
+		{
+			const int MAX_KEEP_ALIVE = 2; //in update ticks
+
+			//update path request until there is nothing left to update
+			//or up to maxIters pathfinder iterations have been consumed
+			int iterCount = maxIters;
+
+			for (int i = 0; i < MAX_QUEUE; i++)
+			{
+				PathQuery q = queue[queueHead % MAX_QUEUE];
+
+				//skip inactive requests
+				if (q.Reference == PATHQ_INVALID)
+				{
+					queueHead++;
+					continue;
+				}
+
+				//handle completed request
+				if (q.status == Status.SUCCESS || q.status == Status.FAILURE)
+				{
+					q.KeepAlive++;
+					if (q.KeepAlive > MAX_KEEP_ALIVE)
+					{
+						q.Reference = PATHQ_INVALID;
+						q.status = 0;
+					}
+
+					queueHead++;
+					continue;
+				}
+
+				//handle query start
+				if (q.status == 0)
+				{
+					bool status = navquery.InitSlicedFindPath((int)q.StartRef, (int)q.EndRef, q.StartPos, q.EndPos);
+					
+					if (status)
+						q.status = Status.SUCCESS;
+					else
+						q.status = Status.FAILURE;
+				}
+
+				//handle query in progress
+				if (q.status == Status.IN_PROGRESS)
+				{
+					int iters = 0;
+					bool status = navquery.UpdateSlicedFindPath(iterCount, ref iters);
+
+					if (status)
+						q.status = Status.SUCCESS;
+					else
+						q.status = Status.FAILURE;
+
+					iterCount -= iters;
+				}
+				if (q.status == Status.SUCCESS)
+				{
+					bool status = navquery.FinalizeSlicedFindPath(q.Path, maxPathSize);
+					q.NPath = q.Path.Count;
+
+					if (status)
+						q.status = Status.SUCCESS;
+					else
+						q.status = Status.FAILURE;
+				}
+
+				if (iterCount <= 0)
+					break;
+
+				queueHead++;
+			}
+		}
+
+		public uint Request(uint startRef, uint endRef, Vector3 startPos, Vector3 endPos)
+		{
+			//find empty slot
+			int slot = -1;
+			for (int i = 0; i < MAX_QUEUE; i++)
+			{
+				if (queue[i].Reference == PATHQ_INVALID)
+				{
+					slot = i;
+					break;
+				}
+			}
+			//could not find slot
+			if (slot == -1)
+				return PATHQ_INVALID;
+
+			uint reference = nextHandle++;
+			if (nextHandle == PATHQ_INVALID) nextHandle++;
+
+			PathQuery q = queue[slot];
+			q.Reference = reference;
+			q.StartPos = startPos;
+			q.StartRef = startRef;
+			q.EndPos = endPos;
+			q.EndRef = endRef;
+
+			q.status = 0;
+			q.NPath = 0;
+			q.KeepAlive = 0;
+
+			queue[slot] = q;
+
+			return reference;
+		}
+
+		public Status GetRequestStatus(uint reference)
+		{
+			for (int i = 0; i < MAX_QUEUE; i++)
+			{
+				if (queue[i].Reference == reference)
+					return queue[i].status;
+			}
+			return Status.FAILURE;
+		}
+
+		public bool GetPathResult(uint reference, int[] path, ref int pathSize, int maxPath)
+		{
+			for (int i = 0; i < MAX_QUEUE; i++)
+			{
+				if (queue[i].Reference == reference)
+				{
+					PathQuery q = queue[i];
+					//free request for reuse
+					q.Reference = PATHQ_INVALID;
+					q.status = 0;
+					//copy path
+					int n = Math.Min(q.NPath, maxPath);
+					q.Path.CopyTo(path);
+					pathSize = n;
+
+					queue[i] = q;
+
+					return true;
+				}
+			}
+
+			return false;
 		}
 
 		private struct PathQuery
@@ -57,10 +208,11 @@ namespace SharpNav
 			public uint StartRef, EndRef;
 
 			//result
-			public uint[] Path;
+			public List<int> Path;
 			public int NPath;
 
 			//state
+			public Status status;
 			public int KeepAlive;
 		}
 	}
