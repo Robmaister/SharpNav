3245f67bb3c1435c82d5d2eec34029115e02fdd3
Robert Rouhani <robert.rouhani@gmail.com>
2014-05-31 16:22:37 -0400

    Added shell for OBJ loader. JSON unit test still fails.

---
+++ SharpNavEditor/IO/ObjLoader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpNavEditor.IO
+{
+	public class ObjLoader : IModelLoader
+	{
+		public bool SupportsPositions
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool SupportsTextureCoordinates
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool SupportsNormals
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool SupportsTangents
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool SupportsBitangents
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool SupportsColors
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool SupportsAnimation
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool SupportsSkeleton
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool SupportsIndexing
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public int CustomVertexDataTypesCount
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public IModelData LoadModel(string path)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
