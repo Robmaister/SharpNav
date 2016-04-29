fcba3a84a2cc9cbb92892919f049f6d28bc64121
Robert Rouhani <robert.rouhani@gmail.com>
2016-04-29 05:01:31 -0400

    Removed extra dependencies, dropped back down to 4.5.1 for Travis CI

--- Source/SharpNav.GUI/ConfigurationForm.cs
+++ Source/SharpNav.GUI/ConfigurationForm.cs
@@ -4,13 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Drawing.Design;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.Windows.Forms.Design;
 using System.IO;
 
 using SharpNav.Geometry;
